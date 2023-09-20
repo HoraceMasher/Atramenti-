ce1c945f697c0fa4b3ac47558000047fc5c4bf75
Horace M Asher <masherdev@gmail.com>
2023-09-21 02:34:43 +0300

    first commit

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ document.addEventListener("DOMContentLoaded", function () {
         carouselInstance.next();
     });
 });
+
+
+    // Function to handle the intersection
+    function handleIntersection(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate__animated", "animate__fadeInUp");
+                observer.unobserve(entry.target);
+            }
+        });
+    }
+
+    // Create an Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, {
+        root: null, // Use the viewport as the root
+        rootMargin: "0px", // No margin
+        threshold: 0.2, // 20% of the target element must be visible
+    });
+
+    // Observe each element with the "statistic" class
+    const statisticElements = document.querySelectorAll(".statistic");
+    statisticElements.forEach((element) => {
+        observer.observe(element);
+    });
