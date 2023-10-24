450e0f70ece8e3ceb60a96024504fb973997008e
Horace M Asher <masherdev@gmail.com>
2023-10-24 13:15:42 +0300

    first commit

--- index.js
+++ index.js
@@ -65,3 +65,21 @@ const cardElements = document.querySelectorAll(".highlight");
 cardElements.forEach((element) => {
     cardObserver.observe(element);
 });
+
+
+const root = document.documentElement;
+const items = Array.from(document.querySelectorAll("li"));
+root.style.setProperty('--active', 0);
+
+items.forEach((item, index) => 
+{
+	if(index === 0) item.setAttribute("data-active", true);
+	item.style.setProperty('--i', index);
+	
+  	item.addEventListener("click", e => 
+	{
+		root.style.setProperty('--active', index);
+		root.querySelectorAll("[data-active]").forEach(el => el.removeAttribute("data-active"));
+    	item.setAttribute("data-active", true);
+  	});
+});
